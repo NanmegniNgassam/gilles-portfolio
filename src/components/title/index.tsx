b4f4fc4b4bd6391f8566bd8d014e2b1ef70da907
Gilles NGASSAM <nanmegningassam@gmail.com>
2024-11-09 06:32:28 +0100

    Animate the Title component

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,9 +1,30 @@
 import styles from "./Title.module.css"
 import { TitleWidgetProps } from "./Title.types";
+import { motion } from "framer-motion";
 
 const Title = (props: TitleWidgetProps) => {
+    const slideInVariants = {
+        hidden: {
+            opacity: 0,
+            x: "-50vw",
+        },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 0.75
+            }
+        }
+    }
+
     return (
-        <div className={styles.titleContainer}>
+        <motion.div 
+            className={styles.titleContainer}
+            variants={slideInVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+        >
             <span className={styles.titleHook}>{ props.titleHook }</span>
             <h2 className={styles.actualTitle}>
                 { props.actualTitle }
@@ -18,8 +39,7 @@ const Title = (props: TitleWidgetProps) => {
                     </p>
                 )
             )}
-            
-        </div>
+        </motion.div>
         
     );
 }
